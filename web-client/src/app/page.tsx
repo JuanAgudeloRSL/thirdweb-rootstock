8d2a9cf5073bf5b7d3367b67840601448e78214e
JuanDiego <juan.agudelo@ext.rootstocklabs.com>
2024-06-05 04:31:53 -0500

    🎉 Scaffold

--- web-client/src/app/page.tsx
+++ web-client/src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
+import { ConnectButton, TransactionButton } from "thirdweb/react";
 import thirdwebIcon from "@public/thirdweb.svg";
 import { client } from "./client";
+import { prepareContractCall } from "thirdweb";
 
 export default function Home() {
   return (
@@ -18,8 +19,6 @@ export default function Home() {
             }}
           />
         </div>
-
-        <ThirdwebResources />
       </div>
     </main>
   );
@@ -54,45 +53,5 @@ function Header() {
   );
 }
 
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
 
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
 
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
-    </div>
-  );
-}
-
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
-  );
-}
